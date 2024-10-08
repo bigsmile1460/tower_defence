e052200e33c0277d86d35f2e431e5e9dcc57e6ca
applaud13 <applaud13@gmail.com>
2024-10-08 22:46:12 +0900

    최초배포
    MYSQL DB 연결
    프리즈마 생성
    socket io 생성

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import { createServer } from "http";
+import initSocket from "./init/socket.js";
+import dotenv from "dotenv";
+
+const app = express();
+const server = createServer(app);
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
+initSocket(server);
+
+server.listen(PORT, async () => {
+  console.log(`서버가 ${process.env.PORT}로 시작됩니다.`);
+});
