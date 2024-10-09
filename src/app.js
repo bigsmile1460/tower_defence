b0108b959f182b35204374f42cb6409eecd638b1
applaud13 <applaud13@gmail.com>
2024-10-09 14:51:06 +0900

    프리즈마 오류 해결
    prismaAsset
    prismaUser
    두개로 나누어서 각각 사용가능
    
    nodemon 추가 (공통)

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import { createServer } from "http";
 import initSocket from "./init/socket.js";
 import dotenv from "dotenv";
+import towerAsset from "./tower/towerAsset.js";
 
 const app = express();
 const server = createServer(app);
@@ -13,6 +14,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
 initSocket(server);
+
+app.use("/api", [towerAsset]);
 
 server.listen(PORT, async () => {
   console.log(`서버가 ${process.env.PORT}로 시작됩니다.`);
--- src/lib/utils/prisma/index.js
+++ src/lib/utils/prisma/index.js
@@ -1,7 +1,24 @@
 import { PrismaClient } from "@prisma/client";
-// 외부에서 prisma를 가져다 쓰기만 하더라도 우리는 실제로 prisma에 적용되어 있는
-// 모델 정보들이나 테이블 정보들을 바탕으로 실제 데이터베이스와 통신을 할 수 있게 됩니다.
-export const prisma = new PrismaClient({
-    log: ["query", "info", "warn", "error"],
-    errorFormat: "pretty",
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const prismaUser = new PrismaClient({
+  log: ["query", "info", "warn", "error"],
+  errorFormat: "pretty",
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL_USER,
+    },
+  },
+});
+
+export const prismaAsset = new PrismaClient({
+  log: ["query", "info", "warn", "error"],
+  errorFormat: "pretty",
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL_ASSET,
+    },
+  },
 });
