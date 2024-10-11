4a8a58736b1da488ad9542123fc02282244366d0
bigsmile1460 <wns4711@naver.com>
2024-10-11 10:26:05 +0900

    update: main 병합 후 몬스터 생성 호출 추가(추후 변경 예정)

--- public/src/game.js
+++ public/src/game.js
@@ -54,6 +54,8 @@ async function GameStart() {
       elpsedTime: Date.now(), // 변경 시간
     });
 
+    //몬스터 생성 호출, todo: 임시로 호출만 테스트. 추후 변경예정
+    UserSocket.GetInstance().SendEvent(6,{})
     // 몬스터 생성 주기
     setInterval(() => {
       game.SpawnMonster();
@@ -62,52 +64,4 @@ async function GameStart() {
     game.GameLoop();
   });
 }
-<<<<<<< HEAD
 
-function initGame() {
-  if (isInitGame) {
-    return;
-  }
-
-  monsterPath = path.generateRandomMonsterPath(); // 몬스터 경로 생성
-  initMap(); // 맵 초기화 (배경, 몬스터 경로 그리기)
-  placeInitialTowers(); // 설정된 초기 타워 개수만큼 사전에 타워 배치
-  placeinhibitor(); // 기지 배치
-
-  //몬스터 스폰 이벤트
-  console.log("스폰 이벤트 ")
-  const result = userSocketSave.SendEvent(6,userSocketSave.stages)
-  console.log("result: ", result)
-
-  setInterval(() => {
-    spawnMonster()
-  }, monsterSpawnInterval);
-  // setInterval(spawnMonster, monsterSpawnInterval); // 설정된 몬스터 생성 주기마다 몬스터 생성
-
-  // 지금 게임 시작 전에 데이터를 불러오는게 제대로 안되는 중
-  setTimeout(() => {
-    userGold = userSocketSave.initGameDB.startGold;
-    inhibitorHp = userSocketSave.initGameDB.inhibitorHp;
-    highScore = userSocketSave.initGameDB.serverHighScore;
-
-    gameLoop(); // 게임 루프 최초 실행
-  }, 1000);
-
-  isInitGame = true;
-}
-
-
-const buyTowerButton = document.createElement("button");
-buyTowerButton.textContent = "타워 구입";
-buyTowerButton.style.position = "absolute";
-buyTowerButton.style.top = "10px";
-buyTowerButton.style.right = "10px";
-buyTowerButton.style.padding = "10px 20px";
-buyTowerButton.style.fontSize = "16px";
-buyTowerButton.style.cursor = "pointer";
-
-buyTowerButton.addEventListener("click", placeNewTower);
-
-document.body.appendChild(buyTowerButton);
-=======
->>>>>>> 44b687fa500fdba325b1030eb0ae15159b98ab82
