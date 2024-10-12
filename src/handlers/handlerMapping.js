37ac3c955e600ac2a15c23806104201ac98a84ed
bigsmile1460 <wns4711@naver.com>
2024-10-12 09:00:27 +0900

    fix: 몬스터 기능 구현중.

--- src/handlers/handlerMapping.js
+++ src/handlers/handlerMapping.js
@@ -1,6 +1,6 @@
 import { towerAttack } from "./tower/towerAttack.js";
 import { stageChange, stageEnd, stageStart } from "./stage/stage.handler.js";
-import { spawnNormal } from "./monster/spawnNormal.js";
+import { monsterCycle, spawnNormal } from "./monster/spawnNormal.js";
 
 const handlerMapping = {
   1: stageStart,
@@ -13,6 +13,7 @@ const handlerMapping = {
   //   8: towerUpgrade,
   //   9: towerBuy,
   //   10: towerSell,
+  11: monsterCycle
 };
 
 export default handlerMapping;
