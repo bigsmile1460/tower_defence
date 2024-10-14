4b73171611f8f66a55016278c589e76b5eac9d9e
MinjaeK <93378322+KonnoYuuki2@users.noreply.github.com>
2024-10-14 23:56:27 +0900

    Update: stageEnd
    1. 스테이지 엔딩 홈페이지 추가

--- public/src/clientHandler/stages/stageEnd.js
+++ public/src/clientHandler/stages/stageEnd.js
@@ -1,8 +1,4 @@
 // 게임 종료: 화면 새로고침
 export const stageEnd = (payload) => {
-  if(payload)
-    alert(payload)
-  console.log("게임오버")
-  
-  location.reload();
+  location.replace(`/stageEnd.html`);
 };
