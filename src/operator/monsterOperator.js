c2413f32e38d36ed6f16f1251c7c9cbbdee28f99
bigsmile1460 <wns4711@naver.com>
2024-10-15 16:42:53 +0900

    fix: 스테이지 클리어 조건 확인 변수를 배열로 변경.코드 정리. 주석 추가

--- src/operator/monsterOperator.js
+++ src/operator/monsterOperator.js
@@ -7,8 +7,8 @@ import {
 } from "../Storages/stage.storage.js";
 import stageOperator from "./stageOperator.js";
 
-export let intervalId = [];//몬스터 스폰
-export let isStageClear; // 스테이지 클리어 조건 확인
+export let intervalId = []; //몬스터 스폰
+export let isStageClear = []; // 스테이지 클리어 조건 확인
 
 //몬스터 정보 호출
 export const getMonster = (stageId) => {
@@ -22,23 +22,19 @@ export const spawnStart = async (socket, userId) => {
   try {
     //필요 정보 : 유저 고유값(토큰이용), 스테이지 정보
     const stageInfo = getStage(userId).stageInfo;
-
-    let nowStage = getStage(userId).stageInfo.stageId; //초기 스테이지
-    //스테이지 정보에 따라 스폰 몬스터 결정
-    let getMonsterInfo = await getMonster(nowStage);
+    const gameEnd = stageInfo.monsterCountLimit; //게임 종료 조건
+    let nowStage = stageInfo.stageId; //초기 스테이지
+    let getMonsterInfo = await getMonster(nowStage); //현재 스테이지에 알맞는 스폰 몬스터
     let interval = getMonsterInfo[0].cycle; // 몬스터 스폰 주기
-    let intervalStatus = "normal"; // 몬스터 스폰에 적용된 interval 상태
+    let intervalStatus = "normal"; // 억제기 상태 확인 값
 
-    // let intervalId; //몬스터 스폰
-
-    //몬스터 스폰 시작
-    function startInterval() {
+    const startInterval = () => {
       intervalId[userId] = setInterval(async () => {
         //서버에 몬스터 객체 생성
         const monsterData = addMonster(userId, getMonsterInfo[0]);
-        //클라이언트에 스테이지 종료 전달
-        if (stageInfo.monsterCountLimit <= getMonsterLength(userId)) {
-          isStageClear = false;
+        //서버 스테이지에 스테이지 종료 전달
+        if (gameEnd <= getMonsterLength(userId)) {
+          isStageClear[userId] = false;
           stageOperator.stageEnd(socket, userId);
         }
         //클라이언트에 몬스터 객체 전달
@@ -47,11 +43,14 @@ export const spawnStart = async (socket, userId) => {
         }
 
         //다음 스테이지로 넘어감 - getStage로 스테이지 정보와 현재 스폰되는 스테이지 비교
-        if (nowStage !== getStage(userId).stageInfo.stageId) {
-          nowStage = getStage(userId).stageInfo.stageId; //현재 스테이지 조회
-          getMonsterInfo = await getMonster(nowStage);
+        const stageCompare = getStage(userId).stageInfo.stageId; //현재 스테이지 조회
+        if (nowStage !== stageCompare) {
+          //스테이지가 변경 되었는지 확인
+          nowStage = stageCompare; //변경된 스테이지로 현재 스테이지 정보 변경
+          getMonsterInfo = await getMonster(nowStage); //스테이지가 변경되면 몬스터 정보 재할당
 
           if (getMonsterInfo.length !== 0) {
+            //스테이지에 해당되는 몬스터가 존재하는지 확인
             if (intervalStatus === "broken") {
               interval = getMonsterInfo[0].cycle / 2; //몬스터 스폰 주기 재설정 - 억제기가 부서졌을 때
             } else {
@@ -62,8 +61,7 @@ export const spawnStart = async (socket, userId) => {
             startInterval(); //몬스터 스폰 시작(재귀)
           } else {
             //스테이지 클리어
-            // 현재 몬스터의 수를 셀 것인가?
-            isStageClear = true;
+            isStageClear[userId] = true;
             clearInterval(intervalId[userId]); // 몬스터 스폰 중지
             stageOperator.stageEnd(socket, userId);
           }
@@ -73,8 +71,8 @@ export const spawnStart = async (socket, userId) => {
           getInhibitorStatus(userId) === "broken" &&
           intervalStatus !== "broken"
         ) {
-          interval /= 2;
-          intervalStatus = "broken";
+          interval /= 2; //스폰 값 2배로 변경
+          intervalStatus = "broken"; //억제기 상태를 "broken"으로 변경
           clearInterval(intervalId[userId]); // 몬스터 스폰 중지
           startInterval(); //몬스터 스폰 시작(재귀)
         }
@@ -83,15 +81,15 @@ export const spawnStart = async (socket, userId) => {
           getInhibitorStatus(userId) === "noraml" &&
           intervalStatus !== "normal"
         ) {
-          interval *= 2;
-          intervalStatus = "normal";
+          interval *= 2; //스폰 값 1배로 변경
+          intervalStatus = "normal"; //억제기 상태를 "normal"로 변경
           clearInterval(intervalId[userId]); // 몬스터 스폰 중지
           startInterval(); //몬스터 스폰 시작(재귀)
         }
       }, interval);
-    }
+    };
 
-    startInterval();
+    startInterval()
   } catch (e) {
     clearInterval(intervalId[userId]);
     throw new Error(e);
